45eadd32ef6f38b46dd9bba534eaa93b04583389
Rezi Nakhutsrishvili <r@rezi.dev>
2021-01-23 04:32:17 +0400

    chore: create .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/prop-types': ['off'],
+    'react/jsx-props-no-spreading': 'off',
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
