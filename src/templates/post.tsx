3ba42a9c8a3b47b048f1184efc3e799906df5156
Rezi Nakhutsrishvili <r@rezi.dev>
2021-01-26 01:54:32 +0400

    feat: add post template

---
+++ src/templates/post.tsx
@@ -0,0 +1,62 @@
+import React, { FunctionComponent } from 'react';
+import { graphql } from 'gatsby';
+import {
+  Box, Heading, Text, useColorModeValue,
+} from '@chakra-ui/react';
+
+import Page from '../components/page';
+
+type Props = {
+  data: {
+    markdownRemark: {
+      html: string;
+      frontmatter: {
+        date: string;
+        title: string;
+        spoiler: string;
+      };
+    }
+  };
+}
+
+const BlogTemplate: FunctionComponent<Props> = ({ data }) => {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
+  const { date, title, spoiler } = frontmatter;
+  return (
+    <Page meta={{ title, description: spoiler }}>
+      <Box mb={8}>
+        <Text color={useColorModeValue('gray.600', 'gray.400')} fontWeight={500}>{date}</Text>
+        <Heading size="xl" fontWeight={600} lineHeight={1.5}>{title}</Heading>
+      </Box>
+      <Box
+        sx={{
+          a: {
+            color: 'blue.500',
+            textDecoration: 'underline',
+            fontWeight: 600,
+            ':hover': {
+              textDecoration: 'none',
+            },
+          },
+        }}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </Page>
+  );
+};
+
+export default BlogTemplate;
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        spoiler
+      }
+    }
+  }
+`;
