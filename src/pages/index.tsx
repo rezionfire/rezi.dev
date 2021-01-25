b4510e94abd39d33813f5b2fba5148b1374b4b6c
Rezi Nakhutsrishvili <r@rezi.dev>
2021-01-26 01:55:11 +0400

    feat: homepage

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import {
+  Box, Heading, Stack, Text, useColorModeValue,
+} from '@chakra-ui/react';
+
+import Page from '../components/page';
+
+type Edges = {
+  node: {
+    frontmatter: {
+      slug: string;
+      title: string;
+      spoiler: string;
+    }
+  }
+};
+
+type Props = {
+  data: {
+    allMarkdownRemark: {
+      edges: Edges[];
+    }
+  };
+};
+
+const IndexPage = ({ data }: Props) => (
+  <Page meta={{ title: 'hello', description: 'world' }}>
+    <Box>
+      <Heading size="xl" mb={4}>Blog</Heading>
+      <Stack direction="column" spacing={6}>
+        {data.allMarkdownRemark.edges.map(({ node }) => {
+          const { frontmatter } = node;
+          const { slug, title, spoiler } = frontmatter;
+
+          return (
+            <Link to={slug} key={title}>
+              <Stack direction="column" spacing={2}>
+                <Heading size="md" fontWeight={600} lineHeight={1.5}>
+                  {title}
+                </Heading>
+                <Text color={useColorModeValue('gray.600', 'gray.400')} fontWeight={500}>{spoiler}</Text>
+              </Stack>
+            </Link>
+          );
+        })}
+      </Stack>
+    </Box>
+  </Page>
+);
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            spoiler
+          }
+        }
+      }
+    }
+  }
+`;
